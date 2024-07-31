f96595ca87ed86cbdd48d6a08ba17002d9d02c03
abha <saxenaabhaaashi@gmail.com>
2024-07-31 21:44:01 +0530

    feat: added static page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
-import { Banners } from "@/components/landing-page/banners";
-import { BestSellingProductList } from "@/components/landing-page/best-selling-product-list";
-import { OvalCardList } from "@/components/landing-page/catagory-list";
-import styled from "styled-components";
+// import { Banners } from "@/components/landing-page/banners";
+// import { BestSellingProductList } from "@/components/landing-page/best-selling-product-list";
+// import { OvalCardList } from "@/components/landing-page/catagory-list";
+// import styled from "styled-components";
 
-const LandingPageWrapper = styled.div`
-display:flex;
-flex-direction:column;
-gap:32px;
-align-items:left;
-`
+// const LandingPageWrapper = styled.div`
+// display:flex;
+// flex-direction:column;
+// gap:32px;
+// align-items:left;
+// `
+
+// export default function Home() {
+//   return (
+//     <LandingPageWrapper>
+//       <Banners />
+//       <OvalCardList sectionTitle="Popular Categories" dividerText="Shop By Categories" id={"categories"} filters={{ "filters[tags][tagName][$in]": "Popular Category" }} />
+//       <BestSellingProductList />
+//       <OvalCardList sectionTitle="We Are Available at" dividerText="Shop By Places" clickable={false} id={"service-available-ats"} />
+//     </LandingPageWrapper>
+//   )
+// }
 
 export default function Home() {
-  return (
-    <LandingPageWrapper>
-      <Banners />
-      <OvalCardList sectionTitle="Popular Categories" dividerText="Shop By Categories" id={"categories"} filters={{ "filters[tags][tagName][$in]": "Popular Category" }} />
-      <BestSellingProductList />
-      <OvalCardList sectionTitle="We Are Available at" dividerText="Shop By Places" clickable={false} id={"service-available-ats"} />
-    </LandingPageWrapper>
-  )
+
+  return <div>Welcome to the home page</div>
 }
